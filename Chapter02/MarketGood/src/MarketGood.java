8f6ad90c9712f88a79540c0c894c4e8ec4765886
aroco <arocozy@gmail.com>
2022-02-25 22:18:18 +0900

    Create MarketGood

---
+++ Chapter02/MarketGood/src/MarketGood.java
@@ -0,0 +1,42 @@
+public class MarketGood {
+    private String name;   // 제품명
+    private int retailPrice;   // 정가
+    private int discountRate;
+
+
+    public MarketGood(String name, int retailPrice, int discountRate) {
+        this.name = name;
+        this.retailPrice = retailPrice;
+        if (discountRate < 0 || discountRate > 100) {
+            this.discountRate = 0;
+        } else {
+            this.discountRate = discountRate;
+        }
+    }
+
+    public MarketGood(String name, int retailPrice) {
+        this.name = name;
+        this.retailPrice = retailPrice;
+        this.discountRate = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setDiscountRate(int rate) {
+        this.discountRate = rate;
+    }
+
+    public int getDiscountRate() {
+        return discountRate;
+    }
+
+    public int getDiscountedPrice() {
+        return (int) (retailPrice * ((double) (100 - discountRate) / 100));
+    }
+}
