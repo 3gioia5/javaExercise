a1fe92c34e24814727372c6e239bb1a16972d4ab
aroco <arocozy@gmail.com>
2022-03-01 22:48:43 +0900

    Modify Theater and Create GradeCalculator

--- Chapter02/Theater/src/Theater.java
+++ Chapter02/Theater/src/Theater.java
@@ -11,7 +11,7 @@ public class Theater {
         // 좌석 배열 생성
         seats = new Seat[rowCount][colCount];
 
-        // 각 seat 인스턴스 생성성
+        // 각 seat 인스턴스 생성
         for (int i = 0; i < rowCount; i++) {
             for (int j = 0; j < colCount; j++) {
                 seats[i][j] = new Seat();
@@ -20,6 +20,85 @@ public class Theater {
 
         this.rowCount = rowCount;
         this.colCount = colCount;
+    }
+
+    public boolean reserve(String name, char rowChar, int col, int numSeat) {
+        int row = getRowIndex(rowChar) + 1;
+
+        if (row > rowCount || col > colCount || col + numSeat - 1 > colCount) {
+            return false;
+        }
+
+        for (int i = 0; i < numSeat; i++) {
+            Seat current = seats[row - 1][col - 1 + i];
+
+            if (current.isOccupied()) {
+                for (int j = 0; j < i; j++) {
+                    seats[row - 1][col - 1 + j].cancel();
+                }
+                return false;
+            }
+
+            current.reserve(name);
+        }
+
+        return true;
+    }
+
+    public int cancel(String name) {
+        int canceledSeat = 0;
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                Seat seat = seats[i][j];
+
+                if (seat.isOccupied() && seat.match(name)) {
+                    seat.cancel();
+                    canceledSeat++;
+                }
+            }
+        }
+
+        return canceledSeat;
+    }
+
+    public int cancel(char rowChar, int col, int numSeat) {
+        int row = getRowIndex(rowChar) + 1;
+
+        if (row > rowCount || col > colCount) {
+            return 0;
+        }
+
+        if (col + numSeat - 1 > colCount) {
+            numSeat = colCount - col + 1;
+        }
+
+        int canceledSeat = 0;
+
+        for (int i = 0; i < numSeat; i++) {
+            if (seats[row - 1][col - 1 + i].isOccupied()) {
+                seats[row - 1][col - 1 + i].cancel();
+                canceledSeat++;
+            }
+        }
+
+        return canceledSeat;
+    }
+
+    public int getNumberOfReservedSeat() {
+        int reservedSeat = 0;
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                Seat seat = seats[i][j];
+
+                if (seat.isOccupied()) {
+                    reservedSeat++;
+                }
+            }
+        }
+
+        return reservedSeat;
     }
 
     public void printSeatMatrix() {
@@ -41,5 +120,9 @@ public class Theater {
             }
             System.out.println();
         }
+    }
+
+    private int getRowIndex(char uppercaseChar) {
+        return uppercaseChar - 'A';
     }
 }
